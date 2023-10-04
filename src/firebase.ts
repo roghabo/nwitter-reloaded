c2a5220d62f99b451cba63f629b9890d0162784d
Jinyound Park <roghabo@gmail.com>
2023-10-04 17:20:07 +0900

    firebase setting

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC33xSm8vI1YyyLPbQi1oz--IGW27-H1Fk",
+  authDomain: "nwitter-reloaded-cc951.firebaseapp.com",
+  projectId: "nwitter-reloaded-cc951",
+  storageBucket: "nwitter-reloaded-cc951.appspot.com",
+  messagingSenderId: "1004307077924",
+  appId: "1:1004307077924:web:6283856f03c8506d6df1b1"
+};
+
+const app = initializeApp(firebaseConfig);
